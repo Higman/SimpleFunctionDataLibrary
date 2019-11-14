21de30d1d8bf459bd8fe34faf962dfea67580713
Higman <s19g451@stu.kagawa-u.ac.jp>
2019-11-14 10:31:41 +0900

    add/Add toString method in DataWithHistory Class

--- src/main/kotlin/com/github/Higman/simple_functiondata_library/DataWithHistory.kt
+++ src/main/kotlin/com/github/Higman/simple_functiondata_library/DataWithHistory.kt
@@ -25,6 +25,28 @@ class DataWithHistory<T>(currentData : T? = null) {
          */
         get() = _history.runCatching { last() }.getOrNull() ?: throw NoExistHistoryElementException()
     fun history(): List<T> = _history.toList()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as DataWithHistory<*>
+
+        if (_history != other._history) return false
+        if (_data != other._data) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = _history.hashCode()
+        result = 31 * result + (_data?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "DataWithHistory(data=$_data)"
+    }
 }
 
 class NoExistHistoryElementException : RuntimeException()
