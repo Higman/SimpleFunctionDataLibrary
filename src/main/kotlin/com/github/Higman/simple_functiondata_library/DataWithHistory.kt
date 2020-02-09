d48093cf32130c1f0ffea9ccffc67e7a72410233
helmsman <higman8sdkfz166>
2020-02-10 01:08:44 +0900

    add/add clone() on DataWithHistory class

--- src/main/kotlin/com/github/Higman/simple_functiondata_library/DataWithHistory.kt
+++ src/main/kotlin/com/github/Higman/simple_functiondata_library/DataWithHistory.kt
@@ -2,8 +2,8 @@ package com.github.Higman.simple_functiondata_library
 
 import java.lang.RuntimeException
 
-class DataWithHistory<T>(currentData : T? = null) {
-    private val _history: MutableList<T> = mutableListOf()
+class DataWithHistory<T>(currentData : T? = null): Cloneable {
+    private var _history: MutableList<T> = mutableListOf()
     private var _data : T? = null
         set(value) {
             field = value
@@ -26,6 +26,22 @@ class DataWithHistory<T>(currentData : T? = null) {
         get() = _history.runCatching { last() }.getOrNull() ?: throw NoExistHistoryElementException()
     fun history(): List<T> = _history.toList()
 
+    override fun toString(): String {
+        return "DataWithHistory(data=$_data)"
+    }
+
+    override fun clone(): DataWithHistory<T> {
+        val dwh = kotlin.runCatching {
+            super.clone() as DataWithHistory<T>
+        }.fold(
+            onSuccess = { it },
+            onFailure = { throw RuntimeException("Failed to clone in ${this::class.qualifiedName}")}
+        )
+        dwh._history = _history.toMutableList()
+
+        return dwh
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -33,19 +49,12 @@ class DataWithHistory<T>(currentData : T? = null) {
         other as DataWithHistory<*>
 
         if (_history != other._history) return false
-        if (_data != other._data) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = _history.hashCode()
-        result = 31 * result + (_data?.hashCode() ?: 0)
-        return result
-    }
-
-    override fun toString(): String {
-        return "DataWithHistory(data=$_data)"
+        return _history.hashCode()
     }
 }
 
